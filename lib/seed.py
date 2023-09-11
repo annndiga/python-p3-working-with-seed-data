2dcfe5187f73d7b173014c6b5906c01d0c88fe3d
Ann Ndiga <annndiga@gmail.com>
2023-09-11 16:47:21 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -8,6 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from models import Game
 
 fake = Faker()
+botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
 
 if __name__ == '__main__':
     
